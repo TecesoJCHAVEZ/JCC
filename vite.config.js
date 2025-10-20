93f9c9a4864400c76bfdb23cdfcf57e3d76f633d
Jose Chavez Chavez <tecesoj@gmail.com>
2025-10-19 19:05:46 -0600

    Fix: Configurar base path en Vite para resolver error 404 en Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // *** Línea CRUCIAL añadida para GitHub Pages ***
+  base: '/JCC/', 
   plugins: [react()],
 })
