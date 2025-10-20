bc833981d203d583e4f58a11d353d2fdd3cf892b
Jose Chavez Chavez <tecesoj@gmail.com>
2025-10-19 19:49:40 -0600

    Final: Ruta Correcta

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // *** Línea CRUCIAL añadida para GitHub Pages ***
-  base: './', 
+  // Usar la ruta completa que incluye /JCC/
+  base: '/JCC/dist/', 
   plugins: [react()],
 })
