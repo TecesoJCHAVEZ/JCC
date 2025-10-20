5138374ef36986355647f9e3e912ee06057192b4
Jose Chavez Chavez <tecesoj@gmail.com>
2025-10-19 19:35:54 -0600

    Final: Rutas relativas

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // *** Línea CRUCIAL añadida para GitHub Pages ***
-  base: '/JCC/', 
+  base: './', 
   plugins: [react()],
 })
